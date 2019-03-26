8924213332c3a72cb66816f68aba07c7b3a27b96
Yongli Jiang <yojiang@microstrategy.com>
2019-03-26 14:38:50 +0800

    update to support 1.1.1b

--- build-libssl.sh
+++ build-libssl.sh
@@ -25,7 +25,7 @@ set -u
 # SCRIPT DEFAULTS
 
 # Default version in case no version is specified
-DEFAULTVERSION="1.1.0i"
+DEFAULTVERSION="1.1.1b"
 
 # Default (=full) set of architectures (OpenSSL <= 1.0.2) or targets (OpenSSL >= 1.1.0) to build
 #DEFAULTARCHS="ios_x86_64 ios_arm64 ios_armv7s ios_armv7 tv_x86_64 tv_arm64 mac_x86_64"
