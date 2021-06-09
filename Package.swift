f4b270fd169159902828f04560ac866cfb0eb2f0
jechague <jechague@genexus.com>
2021-06-09 08:47:11 -0300

    Update binary target name
    In Xcode 13 (beta 1), the binary target name should match that of the resulting XCFramework.
    
    This change is backwards compatible with Xcode 12.x

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GXOpenSSL",
-            targets: ["OpenSSL"]),
+            targets: ["openssl"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.binaryTarget(
-			name: "OpenSSL",
+			name: "openssl",
 			url: "https://github.com/genexuslabs/openssl-apple/releases/download/1.1.1i/openssl.xcframework.zip",
 			checksum: "682ec93b979df0e0de2611e824c9ea5273b5716d4b492fce492837426ad4fa3a"
 		)
